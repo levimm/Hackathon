87539a724ee1cee855d0e788ebb984b218d7c965
Ma <li.ma@hpe.com>
2016-05-22 06:28:32 +0800

    add iterator files

--- services/resourceLocatorService.js
+++ services/resourceLocatorService.js
@@ -1,11 +1,12 @@
 /**
  * Created by shange on 5/21/2016.
  */
-
+ "use strict";
 var CONST = require("../common/global").CONST;
 var Service = require("./service").Service;
 var HOD = require("havenondemand");
-
+var fs = require('fs');
+var path = require('path');
 
 function ResourceLocatorService(){
     //all the training data files should start with trainingData*.txt
@@ -20,12 +21,18 @@ function ResourceLocatorService(){
         /*
          * Get a suggestion list base on the given topic, the sentiment type is defined in the global
          * @param
-         * @return, {filename:string,
-         *           content:string}
+         * @return, [content:string]
          *
          */
-        iterateTrainingData:function *(){
-
+        iterateTrainingData:function(){
+            var contents = [];
+            var files = fs.readdirSync(_dataRootPath);
+            for (let file of files) {
+                if (file.startsWith('trainingData')) {
+                    contents.push(fs.readFileSync(path.join(_dataRootPath, file).toString()));
+                }
+            }
+            return contents;
         },
 
         getHODClient:function(){
